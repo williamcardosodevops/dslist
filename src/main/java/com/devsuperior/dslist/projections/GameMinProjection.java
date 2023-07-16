bf542eec687ef2d5f377362472c0bc7db1eb4b97
williamcardosodevops <williamcardoso2@hotmail.com>
2023-07-15 23:51:15 -0300

    Deploy ready

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjections {
+    Long getId();
+    String getTitle();
+    String getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
